d4d95e0020dd202e9b14ce2668be8a22a1eb3023
sitthinan <sitthinan.mb@outlook.co.th>
2020-12-16 22:42:57 +0700

    menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
 import { Question1Component } from './question1/question1.component';
 import { Question2Component } from './question2/question2.component';
 
 const routes: Routes = [
-  { path: '', component: Question2Component, pathMatch: 'full' },
+  { path: '', component: MenuComponent, pathMatch: 'full' },
+  { path: 'q1', component: Question1Component, pathMatch: 'full' },
+  { path: 'q2', component: Question2Component, pathMatch: 'full' },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { Question1Component } from './question1/question1.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Question2Component } from './question2/question2.component';
 import {HttpClientModule} from '@angular/common/http';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     Question1Component,
-    Question2Component
+    Question2Component,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
